ee2bb1b277c5bbce924ffcb1724f71e964ba788b
Sarah Abu Zeneh <sara.abu.zena2000@gmail.com>
2021-07-15 15:40:23 +0300

    First EX Works Well but the Second one doesnt work

--- ex1.js
+++ ex1.js
@@ -7,17 +7,25 @@ exp
 [9,[5,[10,30]],6]  => [6,[[30,10],5]9]
  
  */
-
+// let reverseArray = [];
 function reverseNestedArray(array) {
     //TODO
-    let reverseArray = [];
-    for(let i = array.length-1 ; i >= 0 ; i--){
-        if (array.length != 0 ){
-            reverseArray.push(array[i]) ;
+    //this for loop to enter the elemetn in the array with if ftatment in
+    // next line to ckeck if this element is an array   
+    for(let i = 0 ; i < array.length ; i++){
+        if (typeof array[i] ==="object"){
+            array[i]= reverseNestedArray(array[i]);
+            // if this element in the array is an array we use the recursive function
+            // to enter the the array in the main array  
         }
-    }   
-    console.log(reverseArray); 
-}
-
-
+    }
+    //here we check if the length of the array doesnt equal zero so we have element in the array 
+    //so we user a bulit-in metof called reverse() used to reverse the order of elements in the array 
+    if (array.length != 0 ) {
+        return array.reverse();
+    }
+}  
+let array = [9,[5,[10,30]],6];
+// reverseNestedArray(array);
+console.log(reverseNestedArray(array));
 module.exports = reverseNestedArray;
--- ex2.js
+++ ex2.js
@@ -3,7 +3,31 @@ in a nested array
 example
 array=[15,6,[5,1],[10,[20,[0]]]]
 contains(array,0)=> return true
-
 contains(array,-1)=> return false 
-
 */
+
+function arrayContaine(array, num){
+    if (array.length == 0)
+    {
+      console.log(1);
+      console.log(array);
+        return false;
+        
+    }else if (typeof array[array.length-1] ==="object"){
+       console.log(2);
+       console.log(array);
+        array[array.length-1] = arrayContaine(array[array.length-1],num);
+        
+    }else{
+        if(array.includes(num) && array.length!=0){
+          console.log(3);
+           console.log(array);
+            return true;
+        }else{
+       
+            return false;
+        }
+    }
+}
+array=[1,2 ,3,[5], [9]];
+console.log(arrayContaine(array, 9));   
